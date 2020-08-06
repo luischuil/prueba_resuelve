64a4f7bb07c81e4276ae862707f0f3c6e148817e
Luis Chuil <luisachc@gmail.com>
2020-08-05 23:03:43 -0500

    initial django repository

---
+++ resuelve/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('ejemplo', views.ResuelveEjemploViewSet.as_view()),
+    #path('', include(router.urls)),
+]
